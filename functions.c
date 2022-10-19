0a0dd4adcb8f23074006c9589f877dbe678f87ba
wilsonosuji <wilsonosuji88@gmail.com>
2022-10-19 09:23:55 -0700

    team task

--- functions.c
+++ functions.c
@@ -4,7 +4,7 @@
 
 /**
  *  * print_char - Prints a char
- *   * @types: Lista of arguments
+ *   * @types: List a of arguments
  *    * @buffer: Buffer array to handle print
  *     * @flags:  Calculates active flags
  *      * @width: Width
@@ -22,7 +22,7 @@ int print_char(va_list types, char buffer[],
 /************************* PRINT A STRING *************************/
 /**
  *  * print_string - Prints a string
- *   * @types: Lista of arguments
+ *   * @types: List a of arguments
  *    * @buffer: Buffer array to handle print
  *     * @flags:  Calculates active flags
  *      * @width: get width.
